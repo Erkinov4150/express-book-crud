35c69cd9ddedcc388c491766f807f9a6d8432e4d
Erkinov4150 <jokerpubg881@gmail.com>
2025-12-29 18:00:50 +0500

    amaliy

--- index.js
+++ index.js
@@ -1,75 +1,210 @@
+// const express = require('express');
+// const app = express();
+// const PORT = 3000;
+
+// app.use(express.json());
+
+// const books = [
+//     {
+//         id: 1,
+//         name: "Qanday qilib dasturlashni organsa boladi",
+//         author: "Erkinov Sardor",
+//         year: 1925
+//     },
+//     {
+//         id: 2,
+//         name: "Jsda cod yozishnni organish",
+//         author: "Erkinov Sardor",
+//         year: 1960
+//     },
+//     {
+//         id: 3,
+//         name: "Backendni organish",
+//         author: "Erkinov Sardor",
+//         year: 1948
+//     }
+
+// ];
+
+// app.get('/books', (req, res) => {
+//     res.json(books);
+// });
+
+// app.post('/books', (req, res) => {
+//     const book = req.body;
+//     books.push(book);
+//     res.status(201).json(book);
+// });
+
+// app.get('/books/:id', (req, res) => {
+//     const id = +req.params.id;
+//     const book = books.find(book => book.id == id);
+//     if (book) {
+//         res.json(book);
+//     } else {
+//         res.status(404).json({ error: 'Kitob topilmadi' });
+//     }
+// });
+
+// app.put('/books/:id', (req, res) => {
+//     const id = +req.params.id;
+//     const book = req.body;
+//     const index = books.findIndex(book => book.id == id);
+//     if (index !== -1) {
+//         books[index] = book;
+//         res.json(book);
+//     } else {
+//         res.status(404).json({ error: 'Kitob topilmadi dedim' });
+//     }
+// });
+
+// app.delete('/books/:id', (req, res) => {
+//     const id = +req.params.id;
+//     const index = books.findIndex(book => book.id == id);
+//     if (index !== -1) {
+//         books.splice(index, 1);
+//         res.sendStatus(204);
+//     } else {
+//         res.status(404).json({ error: 'Kitob topilmadi dedim e qara' });
+//     }
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server yugurvotti ${PORT}-da`);
+// });
+
+
+// const express = require('express');
+// const app = express();
+// const PORT = 3000;
+
+// app.use(express.json());
+// const books = [
+//     {
+//   id: number,
+//   name: string,
+//   author: string,
+//   year: number
+// }
+    
+// ];
+
+// app.post('/books', (req, res) => {
+//     const book = req.body;
+//     books.push(book);
+//     res.status(201).json(book);
+// });
+
+// app.get('/books', (req, res) => {
+//     res.json(books);
+// });
+
+// app.get('/books/:id', (req, res) => {
+//     const id = +req.params.id;
+//     const book = books.find(book => book.id === id);
+//     if (book) {
+//         res.json(book);
+//     } else {
+//         res.status(404).json({ error: 'Kitob topilmadi' });
+//     }
+// });
+
+// app.put('/books/:id', (req, res) => {
+//     const id = +req.params.id;
+//     const book = req.body;
+//     const index = books.findIndex(book => book.id === id);
+//     if (index !== -1) {
+//         books[index] = book;
+//         res.json(book);
+//     } else {
+//         res.status(404).json({ error: 'Kitob topilmadi dedim' });
+//     }
+// });
+
+// app.delete('/books/:id', (req, res) => {
+//     const id = +req.params.id;
+//     const index = books.findIndex(book => book.id === id);
+//     if (index !== -1) {
+//         books.splice(index, 1);
+//         res.sendStatus(204);
+//     } else {
+//         res.status(404).json({ error: 'Kitob topilmadi dedim e qara' });
+//     }
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server yugurvotti ${PORT}-da`);
+// });
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-const books = [
-    {
-        id: 1,
-        name: "Qanday qilib dasturlashni organsa boladi",
-        author: "Erkinov Sardor",
-        year: 1925
-    },
-    {
-        id: 2,
-        name: "Jsda cod yozishnni organish",
-        author: "Erkinov Sardor",
-        year: 1960
-    },
-    {
-        id: 3,
-        name: "Backendni organish",
-        author: "Erkinov Sardor",
-        year: 1948
-    }
+const books = [];
 
-];
+// CREATE
+app.post('/books', (req, res) => {
+    const newBook = {
+        id: books.length + 1,
+        name: req.body.name,
+        author: req.body.author,
+        year: req.body.year
+    };
 
+    books.push(newBook);
+    res.status(201).json(newBook);
+});
+
+// READ ALL
 app.get('/books', (req, res) => {
     res.json(books);
 });
 
-app.post('/books', (req, res) => {
-    const book = req.body;
-    books.push(book);
-    res.status(201).json(book);
-});
-
+// READ ONE
 app.get('/books/:id', (req, res) => {
-    const id = +req.params.id;
-    const book = books.find(book => book.id == id);
-    if (book) {
-        res.json(book);
-    } else {
-        res.status(404).json({ error: 'Kitob topilmadi' });
+    const id = Number(req.params.id);
+    const book = books.find(b => b.id === id);
+
+    if (!book) {
+        return res.status(404).json({ error: 'Kitob topilmadi' });
     }
+
+    res.json(book);
 });
 
+// UPDATE
 app.put('/books/:id', (req, res) => {
-    const id = +req.params.id;
-    const book = req.body;
-    const index = books.findIndex(book => book.id == id);
-    if (index !== -1) {
-        books[index] = book;
-        res.json(book);
-    } else {
-        res.status(404).json({ error: 'Kitob topilmadi dedim' });
+    const id = Number(req.params.id);
+    const index = books.findIndex(b => b.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ error: 'Kitob topilmadi' });
     }
+
+    books[index] = {
+        ...books[index],
+        ...req.body,
+        id
+    };
+
+    res.json(books[index]);
 });
 
+// DELETE
 app.delete('/books/:id', (req, res) => {
-    const id = +req.params.id;
-    const index = books.findIndex(book => book.id == id);
-    if (index !== -1) {
-        books.splice(index, 1);
-        res.sendStatus(204);
-    } else {
-        res.status(404).json({ error: 'Kitob topilmadi dedim e qara' });
+    const id = Number(req.params.id);
+    const index = books.findIndex(b => b.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ error: 'Kitob topilmadi' });
     }
+
+    books.splice(index, 1);
+    res.sendStatus(204);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server yugurvotti ${PORT}-da`);
+    console.log(`Server ${PORT}-portda ishlayapti 🚀`);
 });
-
